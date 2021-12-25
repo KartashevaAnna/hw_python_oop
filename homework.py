2b67bb2f4bb705fdd10e4ec715b68d5307337c32
Anna Kartasheva <annakartashevamail@gmail.com>
2021-12-25 20:36:47 +0300

    'flakehell'

--- homework.py
+++ homework.py
@@ -97,10 +97,12 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
-        spent_calories = ((self.CALORIES_RATIO_1 *
-                           self.get_mean_speed()
-                           - self.CALORIES_RATIO_2) * self.weight
-                          / self.M_IN_KM * self.get_duration_in_minutes()
+        spent_calories = ((self.CALORIES_RATIO_1
+                           * self.get_mean_speed()
+                           - self.CALORIES_RATIO_2)
+                          * self.weight
+                          / self.M_IN_KM
+                          * self.get_duration_in_minutes()
                           )
         return spent_calories
 
